c9fb677836b3ec5bb880060f35de0896c79ee992
Your Name <you@example.com>
2024-02-28 14:34:34 +0300

    add clan param in get all users mapping

--- src/main/java/com/business/money/controllers/UserController.java
+++ src/main/java/com/business/money/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.business.money.controllers;
 
 import com.business.money.DTOs.user.CreateUserDTO;
 import com.business.money.DTOs.user.UserResponseDTO;
+import com.business.money.entities.ClanEntity;
 import com.business.money.entities.UserEntity;
 import com.business.money.exception.exceptions.NotFoundException;
 import com.business.money.exception.exceptions.UserAlreadyExistsException;
 import com.business.money.mappers.UserMapper;
+import com.business.money.services.ClanService;
 import com.business.money.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,9 +22,15 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
+    private final ClanService clanService;
 
     @GetMapping
-    public List<UserResponseDTO> getAllUsers() {
+    public List<UserResponseDTO> getAllUsers(@RequestParam(required = false) String clan) throws NotFoundException {
+        if (clan != null) {
+            ClanEntity clanEntity = clanService.findByName(clan);
+            List<UserEntity> members = clanEntity.getMembers().stream().toList();
+            return members.stream().map(userMapper::toUserResponseDTO).toList();
+        }
         return userService.getAllUsers().stream().map(userMapper::toUserResponseDTO).toList();
     }
 
