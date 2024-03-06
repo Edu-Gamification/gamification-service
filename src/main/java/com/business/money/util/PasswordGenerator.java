f223eee4d591efe419666c12bc505c13ab09c7e4
cuteb1t3h <cuteb1t3h@gmail.com>
2024-03-06 15:45:40 +0300

    added generate

--- src/main/java/com/business/money/util/PasswordGenerator.java
+++ src/main/java/com/business/money/util/PasswordGenerator.java
@@ -8,11 +8,13 @@ import java.security.SecureRandom;
 public class PasswordGenerator {
     private final SecureRandom random = new SecureRandom();
     private final int passwordLength = 10;
-    public String generate(){ String password = random.ints(48, 123)
+    public String generate(){
+        String password = random.ints(48, 123)
             .filter(i -> (i < 58 || i > 64) && (i < 91 || i > 96))
             .limit(passwordLength)
             .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
             .toString();
-        return "sd";
+
+        return password;
     }
 }
